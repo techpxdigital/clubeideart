bef2e42d67c20605bd3c3bd3aff520fbe3343b4d
Gemenson França <gemensonfranca@outlook.com>
2024-07-04 20:40:01 -0300

    api/sdk

--- api/api_pix.php
+++ api/api_pix.php
@@ -11,7 +11,7 @@ curl_setopt_array($curl, array(
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS =>'{
+  CURLOPT_POSTFIELDS => '{
     "description": "Payment for product",
     "external_reference": "MP001",
     "payer": {
@@ -40,13 +40,109 @@ curl_close($curl);
 $obj = json_decode($response);
 
 if (isset($obj->id)) {
-    if ($obj->id != NULL) {
-        
-        $copia_cola   = $obj->point_of_interaction->transaction_data->qr_code;
-        $img_qrcode   = $obj->point_of_interaction->transaction_data->qr_code_base64;
-        $link_externo = $obj->point_of_interaction->transaction_data->ticket_url;
+  if ($obj->id != NULL) {
 
-        echo "<img src='data:image/png;base64, {$img_qrcode}' width='200' /><br/>";
-        echo "<textarea>{$copia_cola}</textarea><br/>";
-    }
+    $copia_cola   = $obj->point_of_interaction->transaction_data->qr_code;
+    $img_qrcode   = $obj->point_of_interaction->transaction_data->qr_code_base64;
+    $link_externo = $obj->point_of_interaction->transaction_data->ticket_url;
+
+    //echo "<img src='data:image/png;base64, {$img_qrcode}' width='200' /><br/>";
+    //echo "<textarea>{$copia_cola}</textarea><br/>";
+  }
 }
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Bottom navbar example · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/navbar-bottom/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
+    <link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
+    <meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+  </head>
+
+  <body>
+    <main class="container" style="margin-top: 5%;">
+      <div class="bg-light p-5 rounded mt-3">
+        <center>
+          <h1><b>Pague sua assinatura com desconto!</b></h1>
+          <p class="lead">Realizando seu pagamento via PIX você tem 10% de desconto na sua assinatura anual.</p>
+          
+          <?php 
+            echo "<img src='data:image/png;base64, {$img_qrcode}' width='400' /></br>"; 
+            echo "<textarea style='margin-top: 30px;'>{$copia_cola}</textarea></br>";
+          ?>
+
+          <a class="btn btn-lg btn-primary" href="/docs/5.0/components/navbar/" style="margin-top: 20px;" role="button">Copie e cole</a>
+        </center>
+      </div>
+    </main>
+    <nav class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Bottom navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+            <li class="nav-item dropup">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown10" data-bs-toggle="dropdown" aria-expanded="false">Dropup</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown10">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    </body>
+</html>
